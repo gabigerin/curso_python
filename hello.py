74f7f8e2b333741a0989846bd92a7ab21625d4aa
gabigerin <gabigerin@gmail.com>
2024-11-18 00:48:05 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -45,3 +45,15 @@ st.write("A quantidade de municípios com localização quilombola é " + str(qt
 
 qtdeComunidades = len(df['NM_AGLOM'].unique())
 st.write("A quantidade de comunidades quilombolas é " + str(qtdeComunidades))
+
+st.header('Número de comunidades por UF')
+st.bar_chart(df['NM_UF'].value_counts())
+
+st.header('Os dez municípios com mais comunidades quilombolas')
+st.bar_chart(df['NM_MUNIC'].value_counts()[:10])
+
+numero = st.slider('Selecione um número de linhas a serem exibidas', min_value = 0, max_value = 100)
+st.write(df.head(numero))
+
+st.metric('# Municípios', len(df['NM_MUNIC'].unique()))
+st.metric('# Comunidades', len(df['NM_AGLOM'].unique()))
