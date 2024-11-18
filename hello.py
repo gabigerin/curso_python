fd41c664aec41ea77326abeae38fddfcd29187eb
gabigerin <gabigerin@gmail.com>
2024-11-18 00:46:29 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -25,3 +25,17 @@ df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR
 
 numero = st.slider('Selecione um número', min_value = 0, max_value = 100)
 st.text("Seu número é " + str(numero))
+
+
+df.drop(columns=['Unnamed: 0'], inplace=True)
+list = ['Lat_d', 'Long_d']
+# convertendo para numeros
+df[list] = df[list].apply(pd.to_numeric, errors='coerce')
+estados = df['NM_UF'].unique()
+estadoFiltro = st.selectbox(
+    'Qual estado selecionar?',
+     estados)
+dadosFiltrados = df[df['NM_UF'] == estadoFiltro]
+if st.checkbox('Mostrar tabela'):
+  st.write(dadosFiltrados)
+st.map(dadosFiltrados, latitude="Lat_d", longitude="Long_d")
