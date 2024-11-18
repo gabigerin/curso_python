b74707f8fc2d56659e6dad64eebd727b213201f5
gabigerin <gabigerin@gmail.com>
2024-11-18 00:30:33 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
 import streamlit as st
 st.write("Sou servidora pública")
+
+
+url = 'https://dadosabertos.camara.leg.br/api/v2/deputados?ordem=ASC&ordenarPor=nome'
+response = req.get(url)
+
+dados = response.json()
+
+import matplotlib.pyplot as plt
+
+df_camara['siglaPartido'].value_counts().plot(kind='bar')
+plt.show()
